148cf691e05043251b0c870b0c43d504b18da525
AlexLeger <alex.m.leger@gmail.com>
2019-02-05 17:33:01 +0100

    Drawing upon BusJacksonIntegrationTests from Spring Cloud Bus repo. Sending to all instances of publisher service ok but JSON conversion issue.

---
+++ user-service/src/main/java/wheelsonthebus/users/controllers/UserController.java
@@ -0,0 +1,53 @@
+package wheelsonthebus.users.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.bus.ServiceMatcher;
+import org.springframework.cloud.bus.jackson.RemoteApplicationEventScan;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.event.EventListener;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wheelsonthebus.users.events.NameEvent;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+@RestController
+@EnableAutoConfiguration
+@SpringBootConfiguration
+@RemoteApplicationEventScan(basePackages = "wheelsonthebus.users.events")
+public class UserController {
+
+    final private Set<String> names = ConcurrentHashMap.newKeySet();
+
+    //@Autowired
+    //private BusJacksonMessageConverter converter; //TODO Find out how it should be done
+
+    @Autowired
+    private ServiceMatcher busServiceMatcher;
+
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
+    @GetMapping("/names")
+    public Collection<String> names() {
+        return this.names;
+    }
+
+    @PutMapping("/names/{name}")
+    public void sayName(@PathVariable String name) {
+        this.names.add(name);
+        this.publisher.publishEvent(
+                new NameEvent(this, this.busServiceMatcher.getServiceId(), name));
+    }
+
+    @EventListener
+    public void handleNameSaid(NameEvent event) {
+        this.names.add(event.getName());
+    }
+}
---
+++ user-service/src/main/java/wheelsonthebus/users/events/NameEvent.java
@@ -0,0 +1,25 @@
+package wheelsonthebus.users.events;
+
+import org.springframework.cloud.bus.event.RemoteApplicationEvent;
+
+public class NameEvent extends RemoteApplicationEvent {
+
+    private String name;
+
+    protected NameEvent() {
+    }
+
+    public NameEvent(Object source, String originService, String name) {
+        super(source, originService);
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
