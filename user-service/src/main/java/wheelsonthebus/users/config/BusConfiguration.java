fa21120ffdffaea1bb2d5b8b19968ac5a31aa141
AlexLeger <alex.m.leger@gmail.com>
2019-02-06 10:12:38 +0100

    Replaced borrowed converter with config class, messages are sent and received by all instances of user-services

---
+++ user-service/src/main/java/wheelsonthebus/users/config/BusConfiguration.java
@@ -0,0 +1,11 @@
+package wheelsonthebus.users.config;
+
+import org.springframework.cloud.bus.jackson.RemoteApplicationEventScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RemoteApplicationEventScan(basePackages = "wheelsonthebus.users.events")
+public class BusConfiguration {
+    // According to documentation, this should register event with the deserializer
+    // Source : https://cloud.spring.io/spring-cloud-bus/single/spring-cloud-bus.html#_registering_events_in_custom_packages
+}
--- user-service/src/main/java/wheelsonthebus/users/controllers/UserController.java
+++ user-service/src/main/java/wheelsonthebus/users/controllers/UserController.java
@@ -4,14 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.bus.ServiceMatcher;
-import org.springframework.cloud.bus.jackson.RemoteApplicationEventScan;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
-import wheelsonthebus.users.borrowed.BusJacksonMessageConverter;
 import wheelsonthebus.users.events.NameEvent;
 
 import java.util.Collection;
@@ -21,13 +19,9 @@ import java.util.concurrent.ConcurrentHashMap;
 @RestController
 @EnableAutoConfiguration
 @SpringBootConfiguration
-@RemoteApplicationEventScan(basePackages = "wheelsonthebus.users.events")
 public class UserController {
 
     final private Set<String> names = ConcurrentHashMap.newKeySet();
-
-    @Autowired
-    private BusJacksonMessageConverter converter; //TODO Find out how it should be done
 
     @Autowired
     private ServiceMatcher busServiceMatcher;
